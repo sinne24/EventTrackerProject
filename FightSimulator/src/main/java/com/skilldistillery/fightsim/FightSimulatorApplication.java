c2b5e59e12084b3ea52a3baf5ac5dac2b9d034ba
Cindy <cindy@Students-MacBook-Pro.local>
2021-10-08 15:46:53 -0600

    Created spring boot project

---
+++ FightSimulator/src/main/java/com/skilldistillery/fightsim/FightSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fightsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightSimulatorApplication.class, args);
+	}
+
+}
