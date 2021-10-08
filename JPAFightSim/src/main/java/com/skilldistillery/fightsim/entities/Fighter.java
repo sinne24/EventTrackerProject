8d3fa1f748f78e84712ed8c63276097d8ce66890
Cindy <cindy@Students-MacBook-Pro.local>
2021-10-08 15:27:13 -0600

    established JPA project and tested entity

---
+++ JPAFightSim/src/main/java/com/skilldistillery/fightsim/entities/Fighter.java
@@ -0,0 +1,60 @@
+package com.skilldistillery.fightsim.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fighter {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String Name;
+
+	public Fighter() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fighter other = (Fighter) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Fighter [id=" + id + ", Name=" + Name + "]";
+	}
+}
